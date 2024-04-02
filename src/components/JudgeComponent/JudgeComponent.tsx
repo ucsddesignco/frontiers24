3375380098ea887a4947b955c7b9a44d054272e4
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2024-04-01 19:26:25 -0700

    feat: add mobile view and fix bugs for judge images

--- src/components/JudgeComponent/JudgeComponent.tsx
+++ src/components/JudgeComponent/JudgeComponent.tsx
@@ -8,69 +8,40 @@ interface JudgeProps {
   imgLink: string;
 }
 
-const JudgeComponent = (props: JudgeProps) => {
+const JudgeComponent = ({
+  name,
+  pronouns,
+  position,
+  funFact,
+  imgLink,
+}: JudgeProps) => {
+  // Convert name to image ID
+  const imageID = name.split(" ").join("_").toLowerCase();
   return (
+    //prettier-ignore
     <div className="judge-card">
-      <svg
-        width="161"
-        height="160"
-        viewBox="0 0 161 160"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <mask id="trapezoidMask">
-            <rect width="161" height="160" fill="black" />
-            <path
-              d="M0 159.231L27.3357 0H161L133.664 159.231H0Z"
-              fill="white"
-            />
-          </mask>
-        </defs>
-        <rect
-          width="161"
-          height="160"
-          mask="url(#trapezoidMask)"
-          fill="url(#imagePattern)"
-        />
-        <pattern
-          id="imagePattern"
-          patternUnits="userSpaceOnUse"
-          width="161"
-          height="160"
-        >
-          {/*
-          <image
-            className="judge-image"
-            href={props.imgLink}
-            x="0"
-            y="0"
-            width="161"
-            height="160"
-  />*/}
-          <image
-            className="judge-image"
-            href={props.imgLink}
-            x="0"
-            y="0"
-            width="161"
-            height="160"
-          />
-        </pattern>
-      </svg>
-      <svg
-        width="95"
-        height="160"
-        viewBox="0 0 121 160"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 159.231L50 0H70L20 159.231H0Z" fill="#ff671e" />
-      </svg>
+      <div className="svg-container">
+        <svg className='svg-image' width="161" height="160" viewBox="0 0 161 160" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <mask id="trapezoidMask">
+              <rect width="161" height="160" fill="black"/>
+              <path d="M0 159.231L27.3357 0H161L133.664 159.231H0Z" fill="white"/>
+            </mask>
+          </defs>
+          <rect width="161" height="160" mask="url(#trapezoidMask)" fill={`url(#${imageID})`}/>
+          <pattern id={imageID} patternUnits="userSpaceOnUse" width="161" height="160">
+            <image href={imgLink} className="judge-image" preserveAspectRatio="xMidYMid slice"/>
+          </pattern>
+        </svg>
+        <svg className='orange-bar' width="37" height="160" viewBox="0 0 37 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M0.664307 159.231H9.66431L37 0H28L0.664307 159.231Z" fill="#FF671E"/>
+        </svg>
+      </div>
       <div className="judge-info">
-        <h3>Name: {props.name}</h3>
-        <p>Pronouns: {props.pronouns}</p>
-        <p>Position: {props.position}</p>
-        <p>Fun Fact: {props.funFact}</p>
+        <h3>{name}</h3>
+        <p>{pronouns}</p>
+        <p>{position}</p>
+        <p className="fun-fact">Fun Fact: {funFact}</p>
       </div>
     </div>
   );
