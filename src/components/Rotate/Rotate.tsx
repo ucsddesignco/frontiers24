2614ea7e2f79e46cd825bcc3b0df68a427ee8209
Brian Liu <brianliu@Brians-MacBook-Pro.local>
2024-04-02 23:55:02 -0700

    Added changes to make pages opacity zero on scroll and make orbit smoother, added mobile media query

--- src/components/Rotate/Rotate.tsx
+++ src/components/Rotate/Rotate.tsx
@@ -13,156 +13,180 @@ function Rotate() {
   gsap.registerPlugin(useGSAP);
   gsap.registerPlugin(ScrollTrigger);
 
+  const mm = gsap.matchMedia();
+
   useGSAP(() => {
-    gsap.to('.one', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-one',
-        scrub: 0.01,
-        start: 'bottom 99%',
-        end: 'bottom'
-      },
-      rotation: -80,
-      transformOrigin: '16% 74%',
-      opacity: 0,
-      duration: 1,
-      ease: 'none'
-    });
+    mm.add('(min-width: 1200px)', () => {
+      //Gsap Scroll Triggers for rotating pages in
 
-    gsap.from('.two', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-two',
-        scrub: 1,
-        start: 'top 99%',
-        end: 'top',
-        markers: {
-          startColor: 'purple',
-          endColor: 'fuchsia',
-          fontSize: '2rem',
-          indent: 100,
-          fontWeight: 'normals'
-        }
-      },
-      x: -340,
-      y: 200,
-      duration: 1,
-      ease: 'in'
-    });
-    gsap.to('.two', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-two',
-        scrub: 0.01,
-        start: 'top 99%',
-        end: 'bottom'
-      },
-      rotation: -80,
-      transformOrigin: '16% 74%',
-      duration: 1,
-      ease: 'none'
-    });
-
-    gsap.from('.three', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-three',
-        scrub: 1,
-        start: 'top 99%',
-        end: 'top'
-      },
-      x: -340,
-      y: 200,
-      duration: 1,
-      ease: 'in'
-    });
-
-    gsap.to('.three', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-three',
-        scrub: 0.01,
-        start: 'top 99%',
-        end: 'bottom'
-      },
-      rotation: -80,
-      transformOrigin: '16% 74%',
-      duration: 1,
-      ease: 'none'
-    });
-
-    gsap.from('.four', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-four',
-        scrub: 1,
-        start: 'top 99%',
-        end: 'top',
-        markers: {
-          startColor: 'purple',
-          endColor: 'fuchsia',
-          fontSize: '2rem',
-          indent: 100,
-          fontWeight: 'normals'
-        }
-      },
-      x: -340,
-      y: 200,
-      duration: 1,
-      ease: 'in'
-    });
-
-    gsap.to('.four', {
-      scrollTrigger: {
-        scroller: '.scroll-cont',
-        trigger: '.scroll-section-four',
-        scrub: 0.01,
-        start: 'top 99%',
-        end: 'bottom'
-      },
-      rotation: -80,
-      transformOrigin: '16% 74%',
-      duration: 1,
-      ease: 'none'
-    });
-
-    /*
-      
-       
-       gsap.to(".three", {
+      gsap.to('.one', {
         scrollTrigger: {
-            trigger: ".three",
-            scrub: 1,
-            start: 'top top',
-            end: '+=50',
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-one',
+          scrub: 0.01,
+          //Where animation starts and ends
+          start: 'bottom 99%',
+          end: 'bottom'
         },
-        rotation: 60,
-        duration: 3,
-        ease: "none",
-       })
-       gsap.to(".three", {
+        rotation: -80,
+        //Transform Origin makes the illusion that the planet is translating
+        transformOrigin: '16% 74%',
+        x: -200,
+        y: -100,
+        opacity: 0,
+        duration: 1,
+        ease: 'none'
+      });
+
+      gsap.from('.two', {
         scrollTrigger: {
-            trigger: ".three",
-            scrub: 1,
-            start: 'bottom bottom',
-            end: '+=50',
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-two',
+          scrub: 0.1,
+          start: 'top 99%',
+          end: 'top',
+          markers: {
+            startColor: 'white',
+            endColor: 'white',
+            fontSize: '18px',
+            fontWeight: 'bold',
+            indent: 20
+          }
         },
-        rotation: -60,
-        duration: 3,
-        ease: "none",
-       })
-       gsap.to(".four", {
+        opacity: 0,
+        duration: 1,
+        x: -1000,
+        y: 200,
+        ease: 'out'
+      });
+
+      gsap.from('.two', {
         scrollTrigger: {
-            trigger: ".four",
-            scrub: 1,
-            start: 'bottom bottom',
-            end: '+=5000',
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-two',
+          scrub: 0.1,
+          //Where animation starts and ends
+          start: 'bottom 99%',
+          end: 'bottom 1%',
+          markers: {
+            startColor: 'red',
+            endColor: 'red',
+            fontSize: '18px',
+            fontWeight: 'bold',
+            indent: 20
+          }
         },
-        rotation: -60,
-        duration: 3,
-        ease: "none",
-       })
-        */
+        opacity: 1,
+        duration: 1,
+        ease: 'none'
+      });
+      gsap.to('.two', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-two',
+          scrub: 0.01,
+          start: 'top 99%',
+          end: 'bottom'
+        },
+        rotation: -80,
+        transformOrigin: '16% 74%',
+        duration: 1,
+        ease: 'none'
+      });
+
+      //Opacity Shifts
+      gsap.from('.three', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-three',
+          scrub: 0.1,
+          start: 'top 99%',
+          end: 'top'
+        },
+        opacity: 0,
+        duration: 1,
+        ease: 'out',
+        x: -1000,
+        y: 200
+      });
+
+      gsap.from('.three', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-three',
+          scrub: 0.1,
+          //Where animation starts and ends
+          start: 'bottom 99%',
+          end: 'bottom'
+        },
+        opacity: 1,
+        duration: 1,
+        ease: 'none'
+      });
+
+      gsap.to('.three', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-three',
+          scrub: 0.01,
+          start: 'top 99%',
+          end: 'bottom'
+        },
+        rotation: -80,
+        transformOrigin: '16% 74%',
+        duration: 1,
+        ease: 'none'
+      });
+
+      //Opacity Shifts
+      gsap.from('.four', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-four',
+          scrub: 0.1,
+          start: 'top 99%',
+          end: 'top'
+        },
+        opacity: 0,
+        duration: 1,
+        ease: 'out',
+        x: -1000,
+        y: 200
+      });
+
+      gsap.from('.four', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-four',
+          scrub: 0.1,
+          //Where animation starts and ends
+          start: 'bottom 100%',
+          end: 'bottom'
+        },
+        opacity: 1,
+        duration: 1,
+        ease: 'none'
+      });
+
+      gsap.to('.four', {
+        scrollTrigger: {
+          scroller: '.scroll-cont',
+          trigger: '.scroll-section-four',
+          scrub: 0.01,
+          start: 'top 99%',
+          end: 'bottom'
+        },
+        rotation: -80,
+        transformOrigin: '16% 74%',
+        duration: 1,
+        ease: 'none'
+      });
+
+      return () => {
+        // optionally return a cleanup function that will be called when none of the conditions match anymore (after having matched)
+        // it'll automatically call context.revert() - do NOT do that here . Only put custom cleanup code here.
+      };
+    });
   });
 
   return (
