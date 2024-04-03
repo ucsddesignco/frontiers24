0d997a5cf699aa0a84d83d4e858b95cef8874995
EdwardNew <pkarrows@gmail.com>
2024-04-02 17:42:53 -0700

    feat: add mobile footer

--- src/pages/Judges/Judges.tsx
+++ src/pages/Judges/Judges.tsx
@@ -1,9 +1,11 @@
 import './Judges.scss';
+import MobileFooter from '../../components/MobileFooter/MobileFooter';
 
 export default function Judges() {
   return (
     <section className="judges">
       <h2>Judges</h2>
+      <MobileFooter />
     </section>
   );
 }
