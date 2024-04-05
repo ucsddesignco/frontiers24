c0baa9e609b25dd8d0abbf25c52eb6786f5cf984
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2024-04-05 12:41:53 -0700

    test: use load event listener to initialize logo dimensions

--- src/components/LogoAndRegister/LogoAndRegister.tsx
+++ src/components/LogoAndRegister/LogoAndRegister.tsx
@@ -23,24 +23,19 @@ export default function LogoAndRegister({
   const lastScrollTopRef = useRef(0);
 
   useEffect(() => {
-    document.fonts.ready.then(() => {
-      const fakeLogoInfo = fakeLogoRef.current?.getBoundingClientRect();
-      // const logoInfo = logoRef.current?.getBoundingClientRect();
-      const initialLogoWidth = fakeLogoInfo?.width || 0;
-      // Change to ref in the future
-      const navRightValue = navRef.current
-        ? parseInt(
-            window.getComputedStyle(navRef.current).getPropertyValue('right')
-          )
-        : 0;
+    const fakeLogoInfo = fakeLogoRef.current?.getBoundingClientRect();
+    //For cleanup
+    const fakeLogoRefCopy = fakeLogoRef.current;
+    // const logoInfo = logoRef.current?.getBoundingClientRect();
+    const initialLogoWidth = fakeLogoInfo?.width || 0;
+    // Change to ref in the future
+    const navRightValue = navRef.current
+      ? parseInt(
+          window.getComputedStyle(navRef.current).getPropertyValue('right')
+        )
+      : 0;
 
-      //DEBUGGING
-      console.log('fake logo info:', fakeLogoInfo);
-
-      const fakeRegisterInfo = fakeRegisterRef.current?.getBoundingClientRect();
-      const registerInfo = registerRef.current?.getBoundingClientRect();
-      const initialRegisterWidth = registerInfo!.width;
-
+    const handleLoad = () => {
       if (logoRef.current && fakeLogoInfo) {
         logoRef.current.style.width = initialLogoWidth + 'px';
         logoRef.current.style.transform = `translate(${fakeLogoInfo.x}px, ${fakeLogoInfo.y - 20}px)`;
@@ -51,38 +46,50 @@ export default function LogoAndRegister({
             0;
         });
       }
+    };
 
-      if (registerRef.current && fakeRegisterInfo) {
-        registerRef.current.style.transform = `translate(${fakeRegisterInfo.x}px, ${fakeRegisterInfo.y}px)`;
-        registerRef.current.style.opacity = '1';
-        setTimeout(() => {
-          (registerRef.current!.style.transition = 'transform 0.4s ease-out'),
-            0;
-        });
-      }
+    fakeLogoRef.current?.addEventListener('load', handleLoad);
 
-      if (scrollContainerRef.current && logoRef.current) {
-        scrollContainerRef.current.addEventListener('scroll', () => {
-          const st = scrollContainerRef.current!.scrollTop;
-          if (st > lastScrollTopRef.current && st > window.innerHeight / 2) {
-            // Scrolling Down
-            const scaleFactor = 0.5;
-            logoRef.current!.style.transform = `translate(${window.innerWidth - (initialLogoWidth * scaleFactor * 1.5 + navRightValue)}px, 20px) scale(${scaleFactor})`;
-            registerRef.current!.style.transform = `translate(${window.innerWidth - (initialRegisterWidth + navRightValue)}px, ${window.innerHeight - (registerInfo!.height + 50)}px)`;
-          } else if (st < window.innerHeight / 2) {
-            // Scrolling Up
-            logoRef.current!.style.scale = '1';
-            if (fakeLogoInfo) {
-              logoRef.current!.style.transform = `translate(${fakeLogoInfo?.x}px, ${fakeLogoInfo?.y - 20}px)`;
-            }
-            if (fakeRegisterInfo) {
-              registerRef.current!.style.transform = `translate(${fakeRegisterInfo?.x}px, ${fakeRegisterInfo?.y}px)`;
-            }
+    //DEBUGGING
+    console.log('fake logo info:', fakeLogoInfo);
+
+    const fakeRegisterInfo = fakeRegisterRef.current?.getBoundingClientRect();
+    const registerInfo = registerRef.current?.getBoundingClientRect();
+    const initialRegisterWidth = registerInfo!.width;
+
+    if (registerRef.current && fakeRegisterInfo) {
+      registerRef.current.style.transform = `translate(${fakeRegisterInfo.x}px, ${fakeRegisterInfo.y}px)`;
+      registerRef.current.style.opacity = '1';
+      setTimeout(() => {
+        (registerRef.current!.style.transition = 'transform 0.4s ease-out'), 0;
+      });
+    }
+
+    if (scrollContainerRef.current && logoRef.current) {
+      scrollContainerRef.current.addEventListener('scroll', () => {
+        const st = scrollContainerRef.current!.scrollTop;
+        if (st > lastScrollTopRef.current && st > window.innerHeight / 2) {
+          // Scrolling Down
+          const scaleFactor = 0.5;
+          logoRef.current!.style.transform = `translate(${window.innerWidth - (initialLogoWidth * scaleFactor * 1.5 + navRightValue)}px, 20px) scale(${scaleFactor})`;
+          registerRef.current!.style.transform = `translate(${window.innerWidth - (initialRegisterWidth + navRightValue)}px, ${window.innerHeight - (registerInfo!.height + 50)}px)`;
+        } else if (st < window.innerHeight / 2) {
+          // Scrolling Up
+          logoRef.current!.style.scale = '1';
+          if (fakeLogoInfo) {
+            logoRef.current!.style.transform = `translate(${fakeLogoInfo?.x}px, ${fakeLogoInfo?.y - 20}px)`;
           }
-          lastScrollTopRef.current = st <= 0 ? 0 : st;
-        });
-      }
-    });
+          if (fakeRegisterInfo) {
+            registerRef.current!.style.transform = `translate(${fakeRegisterInfo?.x}px, ${fakeRegisterInfo?.y}px)`;
+          }
+        }
+        lastScrollTopRef.current = st <= 0 ? 0 : st;
+      });
+
+      return () => {
+        fakeLogoRefCopy?.removeEventListener('load', handleLoad);
+      };
+    }
   }, [fakeLogoRef, scrollContainerRef, fakeRegisterRef, navRef]);
 
   return (
