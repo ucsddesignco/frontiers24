28634263a84d253c6a1631a5c16a5e4d45d3ab09
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2024-04-04 20:10:30 -0700

    Squashed merge with timeline branch

--- src/pages/Timeline/Timeline.tsx
+++ src/pages/Timeline/Timeline.tsx
@@ -1,39 +1,9 @@
 import './Timeline.scss';
+import { TimelineInfo } from './TimelineInfo';
 
 {
   /* Timeline List*/
 }
-const TimlineList = [
-  {
-    time: ' 9:00 AM',
-    title: 'Event Kickoff',
-    description:
-      'Designers will begain with registration and sign-in, an intorduction to the event, and energizing lighting talks'
-  },
-  {
-    time: '10:00 AM',
-    title: 'Sprint Starts + Lunch',
-    description:
-      'Participants will have around 4 hours to design for the given prompt and prepare to present their solution. '
-  },
-  {
-    time: ' 2:00 PM',
-    title: 'Sprint Ends',
-    description:
-      'Teams will present their design solutions to our panel of judges. Finalist presentations begin.'
-  },
-  {
-    time: ' 4:30 PM',
-    title: 'Closing Cenemony',
-    description:
-      '3 winners and 2 honorable mentions announced, prize distribution, and networking session.'
-  },
-  {
-    time: ' 5:00 PM',
-    title: 'Even Ends',
-    description: ''
-  }
-];
 
 type TimelineProps = {
   scroll3Ref: React.RefObject<HTMLElement>;
@@ -46,7 +16,7 @@ export default function Timeline({ scroll3Ref }: TimelineProps) {
         <ul>
           <div>
             <h1 className="title">Timeline</h1>
-            {TimlineList.map((item, index) => (
+            {TimelineInfo.map((item, index) => (
               <li key={index}>
                 <p className="time">{item.time}</p>
                 <div className="orange-line"></div>
